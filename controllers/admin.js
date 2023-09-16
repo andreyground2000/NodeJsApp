ca943bbcc7b42a53ac579bf28beae0ea9ba85774
andreyground2000 <andreygrigoriev555@gmail.com>
2023-09-16 22:06:06 +0200

    Adding product to mongoDb

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,17 +1,17 @@
-const Cart = require('../models/cart');
+// const Cart = require('../models/cart');
 const Product = require('../models/product');
 
-exports.getProducts = (req, res) => {
-  req.user.getProducts()
-    .then(products => {
-      res.render('admin/products', {
-        prods: products,
-        pageTitle: 'Admin Products',
-        path: '/admin/products',
-      });
-    })
-    .catch(err => console.log(err));
-};
+// exports.getProducts = (req, res) => {
+//   req.user.getProducts()
+//     .then(products => {
+//       res.render('admin/products', {
+//         prods: products,
+//         pageTitle: 'Admin Products',
+//         path: '/admin/products',
+//       });
+//     })
+//     .catch(err => console.log(err));
+// };
 
 exports.getAddProduct = (req, res) => {
   res.render('admin/edit-product', {
@@ -27,75 +27,70 @@ exports.postAddProduct = (req, res) => {
   const price = req.body.price;
   const description = req.body.description;
 
-  req.user.createProduct({
-    title,
-    imageUrl,
-    price,
-    description
-  })
-  .then(() => res.redirect('/admin/products')
-  )
-  .catch(err => console.log(err));
+  const product = new Product(title, price , imageUrl, description);
+
+  product.save()
+    .then(() => res.redirect('/admin/products'))
+    .catch(err => console.log(err));
 };
 
-exports.getEditProduct = (req, res, next) => {
-  const editMode = JSON.parse(req.query.edit);
+// exports.getEditProduct = (req, res, next) => {
+//   const editMode = JSON.parse(req.query.edit);
 
-  if (!editMode) {
-    return res.redirect('/');
-  }
+//   if (!editMode) {
+//     return res.redirect('/');
+//   }
 
-  const id = req.params.productId;
+//   const id = req.params.productId;
 
-  Product.findByPk(id)
-  .then(product=> {
-    if (!product) {
-      return res.redirect('/');
-    }
+//   Product.findByPk(id)
+//   .then(product=> {
+//     if (!product) {
+//       return res.redirect('/');
+//     }
 
-    res.render('admin/edit-product', {
-      pageTitle: 'Edit Product',
-      path: '/admin/edit-product',
-      product,
-      editing: editMode
-    });
-  });
-};
+//     res.render('admin/edit-product', {
+//       pageTitle: 'Edit Product',
+//       path: '/admin/edit-product',
+//       product,
+//       editing: editMode
+//     });
+//   });
+// };
 
-exports.postEditProduct = (req, res) => {
-  const {
-    productId,
-    title,
-    price,
-    imageUrl,
-    description 
-  } = req.body;
+// exports.postEditProduct = (req, res) => {
+//   const {
+//     productId,
+//     title,
+//     price,
+//     imageUrl,
+//     description 
+//   } = req.body;
 
-  Product.findByPk(productId)
-    .then(product => {
-      product.title = title;
-      product.price = price;
-      product.imageUrl = imageUrl;
-      product.description = description;
+//   Product.findByPk(productId)
+//     .then(product => {
+//       product.title = title;
+//       product.price = price;
+//       product.imageUrl = imageUrl;
+//       product.description = description;
       
-      return product.save();
-    })
-    .then(() => {
-      res.redirect('/admin/products');
-    })
-    .catch(err => console.log(err))
-};
+//       return product.save();
+//     })
+//     .then(() => {
+//       res.redirect('/admin/products');
+//     })
+//     .catch(err => console.log(err))
+// };
 
-exports.postDeleteProduct = (req, res) => {
-  const { productId, price } = req.body;
+// exports.postDeleteProduct = (req, res) => {
+//   const { productId, price } = req.body;
   
-  Product.destroy({
-    where: {
-      id: productId
-    }
-  })
-  .then(() => {
-    res.redirect('/admin/products');
-  })
-  // Cart.deleteProduct(productId, price);
-}
+//   Product.destroy({
+//     where: {
+//       id: productId
+//     }
+//   })
+//   .then(() => {
+//     res.redirect('/admin/products');
+//   })
+// }
--- models/product.js
+++ models/product.js
@@ -1,30 +1,27 @@
-const { DataTypes } = require('sequelize');
+const getDb = require('../util/database').getDb;
 
-const sequelize = require('../util/database');
-
-const Product = sequelize.define('product', {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  title: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  price: {
-    type: DataTypes.DOUBLE,
-    allowNull: false
-  },
-  imageUrl: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  description: {
-    type: DataTypes.TEXT,
-    allowNull: false
+class Product {
+  constructor(title, price, imageUrl, description) {
+    this.title = title;
+    this.price = price;
+    this.imageUrl = imageUrl,
+    this.description = description;
   }
-});
+
+  save() {
+    const db = getDb();
+
+    return db.collection('products')
+      .insertOne(this)
+      .then(result => {
+        console.log(result);
+        return result;
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+}
+
 
 module.exports = Product;
